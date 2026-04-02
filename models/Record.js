e00224f567cde09b39acb09b2e0577b62317749f
Rahul S <rahulsreekumar03@gmail.com>
2026-04-03 00:23:01 +0530

    feat: create mongodb models

---
+++ models/Record.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+
+const recordSchema = new mongoose.Schema(
+  {
+    amount: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    type: {
+      type: String,
+      enum: ["income", "expense"],
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    date: {
+      type: Date,
+      required: true,
+    },
+    description: {
+      type: String,
+      trim: true,
+      default: "",
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    isDeleted: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+// Exclude soft-deleted records by default
+recordSchema.pre(/^find/, function (next) {
+  this.where({ isDeleted: false });
+  next();
+});
+
+module.exports = mongoose.model("Record", recordSchema);
